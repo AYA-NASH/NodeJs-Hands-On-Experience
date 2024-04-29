ba03267359e3d33e605d33ad36e619af55a2448d
AYA_NASH <aya.nashaatx@gmail.com>
2024-04-29 14:54:35 +0300

    Use bcrypt Package to hash the Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -7,6 +8,14 @@ exports.getLogin = (req, res, next) => {
       isAuthenticated: false
     });
   };
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
 
 exports.postLogin = (req, res, next)=>{
     User.findById('6625555916b00f96126d626f')
@@ -22,6 +31,32 @@ exports.postLogin = (req, res, next)=>{
       console.log(err);
     })
 }
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({email: email})
+  .then(userDoc=>{
+    if(userDoc){
+      return res.redirect('/signup')
+    }
+    return bcrypt.hash(password, 12);})
+  .then(hashedpassword=>{
+    const newUser = new User({
+      email:email,
+      password: hashedpassword,
+      cart: {items: []}
+    })
+    newUser.save()
+  })
+  .then(result=>{
+    return res.redirect('/login')
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+};
 
 exports.postLogout = (req, res, next)=>{
   // clear the session
