350f01efaa9f9e0e92c5179f18137bc8add828b8
AYA_NASH <aya.nashaatx@gmail.com>
2024-04-29 16:02:54 +0300

    Adding login functionality and Protecting the routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,14 +18,25 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next)=>{
-    User.findById('6625555916b00f96126d626f')
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email:email})
     .then(user=>{
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });     
+      if(!user){
+        return res.redirect('/login');  
+      }
+      return bcrypt.compare(password, user.password)
+                  .then(doMatch=>{
+                    if (doMatch){
+                      req.session.isLoggedIn = true;
+                      req.session.user = user;
+                      return  req.session.save(err => {
+                        console.log(err);
+                        res.redirect('/');
+                      });
+                    }
+                    return res.redirect('/login');
+                  })
     })
     .catch(err=>{
       console.log(err);
@@ -41,14 +52,15 @@ exports.postSignup = (req, res, next) => {
     if(userDoc){
       return res.redirect('/signup')
     }
-    return bcrypt.hash(password, 12);})
-  .then(hashedpassword=>{
-    const newUser = new User({
-      email:email,
-      password: hashedpassword,
-      cart: {items: []}
-    })
-    newUser.save()
+    return bcrypt.hash(password, 12)
+      .then(hashedpassword=>{
+        const newUser = new User({
+          email:email,
+          password: hashedpassword,
+          cart: {items: []}
+        })
+        newUser.save()
+      })
   })
   .then(result=>{
     return res.redirect('/login')
--- routes/shop.js
+++ routes/shop.js
@@ -2,23 +2,25 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
-router.get('/', shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+router.get('/', isAuth, shopController.getIndex);
 
-router.get('/products/:productId', shopController.getProduct);
+router.get('/products', isAuth, shopController.getProducts);
 
-router.post('/cart', shopController.postCart);
+router.get('/products/:productId', isAuth, shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+router.post('/cart', isAuth, shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.get('/cart', isAuth, shopController.getCart);
 
-router.get('/orders', shopController.getOrders);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.post('/create-order', shopController.postOrder);
+router.get('/orders', isAuth, shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout);
+router.post('/create-order', isAuth, shopController.postOrder);
+
+router.get('/checkout', isAuth, shopController.getCheckout);
 
 module.exports = router;
