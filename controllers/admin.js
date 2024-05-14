24e042e9d5baa8e1c660958148cd1fe5184ed08d
AYA_NASH <aya.nashaatx@gmail.com>
2024-05-14 11:00:36 +0300

    Validating Add/Edit Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,6 @@
+const {validationResult} = require('express-validator')
 const Product = require('../models/product'); 
+const { errorMonitor } = require('nodemailer/lib/xoauth2');
 
 exports.getAddProduct = (req, res, next)=>{
     res.render('admin/edit-product',
@@ -6,7 +8,8 @@ exports.getAddProduct = (req, res, next)=>{
                     pageTitle: "Add Product",
                     path:'/admin/add-product',
                     editing: false,
-                    isAuthenticated: req.session.loggedIn
+                    hasError: false,
+                    errorMessage: null
                 })
 };
 
@@ -16,7 +19,23 @@ exports.postAddProduct = (req, res, next)=>{
     const price = req.body.price;
     const description = req.body.description;
     const userId = req.user;
-    
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).render('admin/edit-product',
+        {
+            pageTitle: "Add Product",
+            path:'/admin/add-product',
+            editing: false,
+            hasError : true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description
+            },
+            errorMessage: errors.array()[0].msg
+        });
+    }
     const product = new Product({
         title: title,
         imageUrl: imageUrl,
@@ -50,7 +69,8 @@ exports.getEditProduct = (req, res, next)=>{
                     path:'/admin/add-product',
                     editing: editMode,
                     product: product,
-                    isAuthenticated: req.session.loggedIn
+                    hasError: false,
+                    errorMessage: null
                 });
     })
     .catch(err=>{
@@ -65,7 +85,24 @@ exports.postEditProduct = (req, res, next)=>{
     const url = req.body.imageUrl;
     const price = req.body.price;
     const desc = req.body.description;
-
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).render('admin/edit-product',
+        {
+            pageTitle: "Edit Product",
+            path:'/admin/add-product',
+            editing: true,
+            hasError : true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description,
+                _id: prodId
+            },
+            errorMessage: errors.array()[0].msg
+        });
+    }
     Product.findById(prodId)
     .then(product=>{
         product.title = title;
@@ -92,7 +129,8 @@ exports.getProducts = (req, res, next)=>{
                     prods: products,
                     pageTitle: "Admin Products",
                     path:'/admin/products',
-                    isAuthenticated: req.session.loggedIn
+                    hasError: false,
+                    errorMessage: null
                 });
     })
     .catch(err=>{
