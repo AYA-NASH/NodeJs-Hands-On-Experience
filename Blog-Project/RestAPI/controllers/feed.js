b7f1d2ad9332a8c36c59be627b3b63da4bc583ff
AYA_NASH <aya.nashaatx@gmail.com>
2024-05-30 10:23:17 +0300

    Enabling file Uploads

--- Blog-Project/RestAPI/controllers/feed.js
+++ Blog-Project/RestAPI/controllers/feed.js
@@ -2,34 +2,62 @@ const { validationResult } = require("express-validator");
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next)=>{
-    res.status(200).json({
-        posts:[
-            {
-                _id : '1',
-                title: 'First Post',
-                content: 'This is the first post!!',
-                imageUrl: 'Images/cat.jpg',
-                creator: {
-                    name: 'Aya'
-                },
-                createdAt: new Date()
-            }
-        ]
-    });
+    Post.find()
+    .then(posts=>{
+        res.status(200).json({
+            posts: posts
+        });
+    })
+    .catch((err)=>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+    
 };
+
+exports.getPost = (req, res, next)=>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post=>{
+        if(!post){
+            const error = new Error("Post Can't be FOUND!!!");
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({post: post});
+    })
+    .catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+}
 
 exports.creatPost = (req, res, next)=>{
     const errors = validationResult(req)
     const title = req.body.title;
     const content = req.body.content;
     if(!errors.isEmpty()){
-        return res.status(422).json({message: 'Validation Error!', errors: errors.array()})
+        const error = new Error('Validation Error!!!!!!!');
+        error.statusCode = 422;
+        throw error;
     }
+
+    if(!req.file){
+        const error = new Error('No Image Provided');
+        error.statusCode = 422;
+        throw error
+    }
+
+    const imageUrl = req. file.path;
 
     const post = new Post({
         title: title,
         content: content,
-        imageUrl: 'Images/cat.jpg',
+        imageUrl: imageUrl,
         creator:{
             name:'Aya'
         }
@@ -43,6 +71,9 @@ exports.creatPost = (req, res, next)=>{
         })
     })
     .catch(err=>{
-        console.log(err);
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
     })
 }
