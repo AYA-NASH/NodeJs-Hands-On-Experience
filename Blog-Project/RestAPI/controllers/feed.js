be6ca44204b8e73bfeb36b391d5966edca56e0c2
AYA_NASH <aya.nashaatx@gmail.com>
2024-06-06 14:10:48 +0300

    fixing the creator bug, and rewriting with async/await

--- Blog-Project/RestAPI/controllers/feed.js
+++ Blog-Project/RestAPI/controllers/feed.js
@@ -4,31 +4,28 @@ const { validationResult } = require("express-validator");
 const Post = require('../models/post');
 const User = require('../models/user');
 
-exports.getPosts = (req, res, next)=>{
+exports.getPosts = async (req, res, next)=>{
     const currentPage = req.query.page || 1;
     const perPage = 2;
-    let totalItems; // keeps track total items in DB.
-    Post.find()
-    .countDocuments()
-    .then(count=>{
-        totalItems = count;
-        return Post.find()
-            .skip((currentPage - 1) * perPage)
-            .limit(perPage);
-    })
-    .then(posts=>{
+    try{
+        const totalItems = await Post.find().countDocuments();
+        const posts = await Post.find()
+                            .populate('creator')
+                            .skip((currentPage - 1) * perPage)
+                            .limit(perPage);
+
         res.status(200).json({
             message: 'Posts Fetched Successfully.',
             posts: posts,
             totalItems: totalItems
         });
-    })
-    .catch((err)=>{
+    }
+    catch(err){
         if(!err.statusCode){
             err.statusCode = 500;
         }
         next(err);
-    })    
+    }
 };
 
 exports.getPost = (req, res, next)=>{
