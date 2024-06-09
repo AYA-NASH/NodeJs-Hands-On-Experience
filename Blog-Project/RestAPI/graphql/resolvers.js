bd01eab3d62be90b91b4cfd2d211b14194417c79
AYA_NASH <aya.nashaatx@gmail.com>
2024-06-09 19:54:12 +0300

    adding login functionality

--- Blog-Project/RestAPI/graphql/resolvers.js
+++ Blog-Project/RestAPI/graphql/resolvers.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
-const validator = require('validator') 
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 module.exports = {
@@ -8,9 +10,11 @@ module.exports = {
         if(!validator.isEmail(userInput.email)){
             errors.push({message: 'Invalid Email'})
         }
-        if (validator.isEmpty(userInput.password) ||
-            validator.isLength(userInput.password, {min: 5})){
-            errors.push({message:'too short Password'})
+        if (
+            validator.isEmpty(userInput.password) ||
+            !validator.isLength(userInput.password, { min: 5 })
+          ) {
+            errors.push({ message: 'Password too short!' });
         }
         if (errors.length > 0){
             const error = new Error('Invalid Input.')
@@ -31,5 +35,29 @@ module.exports = {
         });
         const createdUser = await user.save();
         return { ...createdUser._doc, _id: createdUser._id.toString() };
-    } 
+    },
+
+    login: async function({ email, password }) {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          const error = new Error('User not found.');
+          error.code = 401;
+          throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+          const error = new Error('Password is incorrect.');
+          error.code = 401;
+          throw error;
+        }
+        const token = jwt.sign(
+          {
+            userId: user._id.toString(),
+            email: user.email
+          },
+          'somesupersecretsecret',
+          { expiresIn: '1h' }
+        );
+        return { token: token, userId: user._id.toString() };
+      }
 }
--- Blog-Project/reactFrontendSetup/src/pages/Feed/Feed.js
+++ Blog-Project/reactFrontendSetup/src/pages/Feed/Feed.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import openSocket from 'socket.io-client';
 
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
@@ -36,18 +35,6 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    const socket = openSocket('http://localhost:8080');
-    socket.connect('posts', data=>{
-      if(data.action === 'create'){
-        this.addPage(data.post);
-      }
-      else if(data.action === 'update'){
-        this.updatePost(data.post);
-      }
-      else if(data.action === 'delete'){
-        this.loadPosts();
-      }
-    })
   }
 
   addPage = post=>{
